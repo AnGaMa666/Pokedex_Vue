3dd79303de211a8a8951a76010de8d4750cfc341
AnGaMa <123359907+AnGaMa666@users.noreply.github.com>
2026-08-05 14:01:35 +0200

    style: restore original pastel type colors

--- src/utils/typeColors.js
+++ src/utils/typeColors.js
@@ -1,25 +1,25 @@
 export const TYPE_COLORS = {
-  bug: '#91a119',
-  dark: '#624d4e',
-  dragon: '#5060e1',
-  electric: '#fac000',
-  fairy: '#ef70ef',
-  fighting: '#ff8000',
-  fire: '#e62829',
-  flying: '#81b9ef',
-  ghost: '#704170',
-  grass: '#3fa129',
-  ground: '#915121',
-  ice: '#3dcef3',
-  normal: '#9fa19f',
-  poison: '#9141cb',
-  psychic: '#ef4179',
-  rock: '#afa981',
-  steel: '#60a1b8',
-  water: '#2980ef',
+  bug: 'lightgreen',
+  dark: 'darkgray',
+  dragon: 'mediumpurple',
+  electric: 'lightyellow',
+  fairy: 'lightpink',
+  fighting: 'lightpink',
+  fire: 'lightcoral',
+  flying: 'lightskyblue',
+  ghost: 'lightsteelblue',
+  grass: 'lightgreen',
+  ground: 'burlywood',
+  ice: 'lightcyan',
+  normal: 'lightgray',
+  poison: 'mediumorchid',
+  psychic: 'lightcoral',
+  rock: 'lightgray',
+  steel: 'lightgray',
+  water: 'lightblue',
 };
 
-export const getTypeColor = (typeName) => TYPE_COLORS[typeName] || '#64748b';
+export const getTypeColor = (typeName) => TYPE_COLORS[typeName] || 'lightgray';
 
 export const getTypeGradient = (types = []) => {
   const primaryColor = getTypeColor(types[0]?.type?.name);
